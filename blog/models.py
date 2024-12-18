eaec5e2a6b3061a5041a3467f854a0e95606c97c
Rahidul Islam <rahiseli@outlook.com>
2024-12-18 21:35:42 +0600

    blog model is renamed

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Blog(models.Model):
+class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
