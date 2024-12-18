803d70d508c5a7da888276d532e35435ecdb3a48
Rahidul Islam <rahiseli@outlook.com>
2024-12-18 20:52:40 +0600

    Installed apps is modifed

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+THIRD_PARTY_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
+]
+
+LOCAL_APPS = []
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
