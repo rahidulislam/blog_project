1e03315e0b166a8fe26a1f0b0ecd3010844185e7
Rahidul Islam <rahiseli@outlook.com>
2024-12-22 22:06:23 +0600

    seo friendly url is generated

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('<int:id>/', post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+
+
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, "blog/post/list.html", {"posts": posts})
 
-def post_detail(request,id):
+
+def post_detail(request, year, month, day, slug):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("Post does not exist")
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post,
+        slug=slug,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, "blog/post/detail.html", {"post": post})
