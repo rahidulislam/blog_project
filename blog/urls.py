cfb44fcf261a932f7cfc2c9b12b10ca9c4c153e5
Rahidul Islam <rahiseli@outlook.com>
2024-12-25 15:26:03 +0600

    comment is added

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, post_detail, post_share
+from .views import PostListView, post_detail, post_share, post_comment
 
 app_name = 'blog'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
+    path('<int:post_id>/comment/', post_comment, name='post_comment'),
 ]
 
