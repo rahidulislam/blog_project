311040e31c1d64cc289cc3e6107e66c0b92f3a4c
Rahidul Islam <rahiseli@outlook.com>
2024-12-26 07:19:22 +0600

    similar post is added

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.conf import settings
+from django.db.models import Count
 from taggit.models import Tag
 from .models import Post
 from .forms import EmailPostForm, CommentForm
@@ -51,11 +52,17 @@ def post_detail(request, year, month, day, slug):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
-
+    # list of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )
+    print(similar_posts)
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {"post": post, "comments": comments, "form": form, "similar_posts": similar_posts},
     )
 
 
